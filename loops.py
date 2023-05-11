22477a150c185af59a4db1de4cc81a05f9bed7ff
Karunwisimi <karunwisimisola1@gmail.com>
2023-05-11 17:28:37 +0100

    first

---
+++ loops.py
@@ -0,0 +1,28 @@
+# nested loops 
+# x = ["red", "blue", "green", "red"]
+# profile = [1, 2, 3, 4]
+# length = len(x) - 2
+# print(x[length])
+
+i = 0
+while i<=10:
+    print(i%3)
+    i += 1
+
+
+    #    print(y)
+    #    break
+
+    #    for d in profile:
+    #     print(d)
+    #     print(y)
+
+# the range function     
+# for x in range(20, 10, -2)
+#     print(x)
+
+# while loop
+# i = 0
+# while i<=6:
+#     print(i)
+#     i += 1 
